ff1bdbdd5c7562f6722105d46097efa6d6a0f3bd
Rafael Domenici <rafaeldomenici@hotmail.com>
2022-07-29 17:59:24 -0300

    pratica 1 versão 3

--- mes-1-logica-de-programacao/tarefa-semana-1/pratica-1.js
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-1.js
@@ -3,3 +3,9 @@ let email = "rafaeldomenici@hotmail.com";
 let profissao = "Professor";
 let idade = 32;
 let lugarQueSonhoConhecer = "Austrália";
+
+console.log("Nome:", nome);
+console.log("Email:", email);
+console.log("Profissão:", profissao);
+console.log("Idade:", idade);
+console.log("Lugar que sonho conhecer:", lugarQueSonhoConhecer);
